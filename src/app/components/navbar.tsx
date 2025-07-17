86b6a6dc7eb9b11a77b63d3c5d12d9e67c66464a
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-07-17 11:49:26 -0300

    feat: add surprise Me

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import SearchComponent from "./SearchComponent";
+import recipes from "../../backend/recipes.json";
 
 const NavBar = () => {
+  const router = useRouter();
+
+  const handleSurpriseMe = () => {
+    // Gera um índice aleatório baseado no número total de receitas
+    const randomIndex = Math.floor(Math.random() * recipes.receitas.length);
+    router.push(`/full-recipe-page/${randomIndex}`);
+  };
+
   return (
     <header className="flex-shrink-0">
       <div className="w-full h-5 bg-brand-yellow"></div>
@@ -16,11 +28,19 @@ const NavBar = () => {
           />
         </div>
         <div className="">
-          <Button className="font-semibold text-lg" variant="ghost">
+          <Button
+            className="font-semibold text-lg"
+            variant="ghost"
+            onClick={() => router.push("/")}
+          >
             Home
           </Button>
-          <Button className="font-semibold text-lg" variant="ghost">
-            Receitas
+          <Button
+            className="font-semibold text-lg"
+            variant="ghost"
+            onClick={handleSurpriseMe}
+          >
+            Me Surpreenda
           </Button>
           <Button className="font-semibold text-lg" variant="ghost">
             Sobre nós
